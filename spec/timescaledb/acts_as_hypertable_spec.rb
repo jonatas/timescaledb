851d303df279ea305f0d118e885dd5e2e7ccf00b
Jônatas Davi Paganini <jonatasdp@gmail.com>
2024-04-09 16:14:52 -0300

    Remove replication factor references

--- spec/timescaledb/acts_as_hypertable_spec.rb
+++ spec/timescaledb/acts_as_hypertable_spec.rb
@@ -41,8 +41,6 @@ RSpec.describe Timescaledb::ActsAsHypertable do
       is_expected.to be_compression_enabled
     end
 
-    its(:replication_factor) { is_expected.to be_nil }
-    its(:data_nodes) { is_expected.to be_nil }
     its(:num_dimensions) { is_expected.to eq(1) }
     its(:tablespaces) { is_expected.to be_nil }
     its(:hypertable_name) { is_expected.to eq(Event.table_name) }
