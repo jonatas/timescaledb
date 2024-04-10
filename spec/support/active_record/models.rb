dc150476ac34666d31c6f8cfa5f3aeb7215759ef
Jônatas Davi Paganini <jonatasdp@gmail.com>
2024-04-09 21:20:22 -0300

    Remove unused code

--- spec/support/active_record/models.rb
+++ spec/support/active_record/models.rb
@@ -23,27 +23,6 @@ class HypertableWithCustomTimeColumn < ActiveRecord::Base
   acts_as_hypertable time_column: :timestamp
 end
 
-class HypertableLean < ActiveRecord::Base
-  self.primary_key = "identifier"
-
-  acts_as_hypertable skip_association_scopes: true, skip_default_scopes: true
-end
-
-class HypertableWithCustomTimeColumnAndLean < ActiveRecord::Base
-  self.table_name = "hypertable_with_custom_time_column_and_lean"
-  self.primary_key = "identifier"
-
-  acts_as_hypertable time_column: :timestamp, skip_association_scopes: true, skip_default_scopes: true
-end
-
-class HypertableWithCustomTimeColumnAndLeanAndNoOptions < ActiveRecord::Base
-  self.table_name = "hypertable_with_custom_time_column_and_lean_and_no_options"
-  self.primary_key = "identifier"
-
-  acts_as_hypertable skip_association_scopes: true, skip_default_scopes: true
-end
-
-
 class HypertableSkipAllScopes < ActiveRecord::Base
   self.table_name = "hypertable_skipping_all_scopes"
   acts_as_hypertable time_column: :timestamp, skip_association_scopes: true, skip_default_scopes: true
